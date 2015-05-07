dd4dbe22792e32c37ebf02c7046e46b235d2e8c9
Nacchofer31 <nachopad31@hotmail.com>
2015-05-07 16:08:16 +0200

    Decada 70 Historia

--- ProyectoInicial/src/vista/Decada70.java
+++ ProyectoInicial/src/vista/Decada70.java
@@ -13,6 +13,8 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 public class Decada70 extends JPanel {
 	
@@ -68,23 +70,34 @@ public class Decada70 extends JPanel {
 		add(button10);
 		
 		//JScrollPane
-		JScrollPane jscroll = new JScrollPane();
-		jscroll.setEnabled(false);
-		jscroll.setBounds(475, 30, 775, 550);
-		jscroll.setBorder(null);
-		this.add(jscroll);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setEnabled(false);
-		textPane.setFont(new Font("BatangChe", Font.PLAIN, 16));
-		textPane.setForeground(Color.WHITE);
-		textPane.setOpaque(false);
-		textPane.setText("Aqui se incorporara \n"
-				+ "el texto de la decada de los 70");
-		jscroll.setViewportView(textPane);
-		
-		jscroll.setOpaque(false);
-		jscroll.getViewport().setOpaque(false);
+				JScrollPane jscroll = new JScrollPane();
+				jscroll.setBounds(475, 30, 775, 550);
+				jscroll.setEnabled(false);
+				jscroll.setBorder(null);
+				this.add(jscroll);
+				
+				JTextPane txtpnDec70 = new JTextPane();
+				txtpnDec70.setEditable(false);
+				txtpnDec70.setEnabled(false);
+				txtpnDec70.setFont(new Font("BatangChe", Font.PLAIN, 17));
+				txtpnDec70.setForeground(Color.WHITE);
+				txtpnDec70.setOpaque(false);
+				txtpnDec70.setText("Ralph Baer y el Magnavox Odyssey\r\n\r\nRalph Baer es considerado por muchos como el inventor de los videojuegos tal y como los conocemos. Es el inventor de las videoconsolas.\r\nEn el a\u00F1o 1966, por su cuenta, construy\u00F3 la primera consola dom\u00E9stica de videojuegos. \u00C9l sab\u00EDa lo que quer\u00EDa hacer pero luch\u00F3 durante a\u00F1os para encontrar empresas o inversores que confiaran en \u00E9l para poner en el mercado su primera consola (Odyssey), cosa que conseguir\u00EDa en el a\u00F1o 1972.\r\n\r\nCon todas las ventas de la consola, se consiguieron vender cerca de 130.000 unidades en la campa\u00F1a de navidad, todo un \u00E9xito, que atrajo la atenci\u00F3n de numerosos emprendedores, entre ellos Nolan Bushnell.\r\n\r\nLa creaci\u00F3n de Atari\r\n\r\nNolan Bushnell junto a Ted Dabney eligieron el nombre de \"Syzygy\" para la futura compa\u00F1\u00EDa, sin embargo, el nombre ya estaba registrado por otra compa\u00F1\u00EDa, por lo que finalmente se decantaron por el nombre de Atari  y se fundo el 1 de Junio de 1972.\r\n\r\nEl nombre de Atari proviene del juego de mesa Go, es la palabra utilizada cuando se consiguen rodear las piedras del oponente y se va a ganar la partida.\r\n\r\nNace el Pong\r\n\r\nCuando Bushnell trabajaba con Nutting, se enteraron de que en la Magnavox Profit Caravan de 1972 se estaba mostrando un sistema dom\u00E9stico de videojuegos, y enviaron a Bushnell para verlo. Fue all\u00ED donde Bushnell vio el juego de ping pong de Baer y probablemente donde se le ocurri\u00F3 la idea de crear un juego similar cuando comenzaba su etapa en Atari.\r\n\r\nBushnell lo llam\u00F3 Pong, fue el mayor \u00E9xito jam\u00E1s visto en la industria de los videojuegos hasta ese momento, cambiando para siempre una industria que dura hasta hoy en d\u00EDa.\r\n\r\nEl rival directo de Atari\r\n\r\nCon el \u00E9xito de Home Pong, cientos de compa\u00F1\u00EDas se lanzaron a crear consolas y dispositivos electr\u00F3nicos para jugar en casa de la misma forma. Magnavox volvi\u00F3 a intentarlo, despu\u00E9s del fracaso de la Odyssey, con la Odyssey 100, otro error. Pero Atari ahora tendr\u00EDa un rival m\u00E1s fuerte: Coleco.\r\n\r\nColeco dise\u00F1\u00F3 una consola durante todo el a\u00F1o 1975: Telstar. Fue lanzada en junio de 1976. No dejaba de ser un Home Pong m\u00E1s, pero tuvo un \u00E9xito brutal ya que Coleco era el principal cliente de General Instrument, la empresa que suministraba los chips para los juegos de consola, y el resto de empresas que lo ped\u00EDan, no dispon\u00EDan de existencias.");
+				
+				SimpleAttributeSet sas = new SimpleAttributeSet(); 
+				StyleConstants.setBold(sas, true);
+				StyleConstants.setItalic(sas, true);
+				StyleConstants.setFontSize(sas, 23);
+				txtpnDec70.getStyledDocument().setCharacterAttributes(0, 32, sas, false);
+				txtpnDec70.getStyledDocument().setCharacterAttributes(665,20, sas, false);
+				txtpnDec70.getStyledDocument().setCharacterAttributes(1091,12, sas, false);
+				txtpnDec70.getStyledDocument().setCharacterAttributes(1632,25, sas, false);
+				jscroll.setViewportView(txtpnDec70);
+				
+				jscroll.setOpaque(false);
+				jscroll.getViewport().setOpaque(false);
+				
+				
 		
 		
 		//IMAGEN DE FONDO
