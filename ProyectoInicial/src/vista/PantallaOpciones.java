bc82db5820180ad575173760276bf1c5b0908395
tony <tonymateo95@gmail.com>
2015-05-07 16:31:23 +0200

    Pantalla opciones V2

--- ProyectoInicial/src/vista/PantallaOpciones.java
+++ ProyectoInicial/src/vista/PantallaOpciones.java
@@ -17,13 +17,24 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+import java.awt.SystemColor;
+import java.awt.Rectangle;
 
 public class PantallaOpciones extends JPanel {
+	private JTextField enlaceWeb;
 
 	public PantallaOpciones() {
 		
 		setLayout(null);
 		setBounds(0, 105, 1355, 591);
+
 		
 		//Boton español
 		JButton esp = new JButton("");
@@ -31,98 +42,127 @@ public class PantallaOpciones extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		esp.setBounds(76, 92, 100, 80);
+		esp.setBounds(492, 62, 100, 80);
 		esp.setContentAreaFilled(false);
 		esp.setBorderPainted(false);
-		add(esp);
+		this.add(esp);
 		
 		//Boton ingles
 		JButton ing = new JButton("");
-		ing.setBounds(350, 92, 100, 80);
+		ing.setBounds(766, 62, 100, 80);
 		ing.setContentAreaFilled(false);
 		ing.setBorderPainted(false);
-		add(ing);
-		
-		//Texto del enlace
-		JLabel textoEnlace = new JLabel("Aplicaci\u00F3n desarrollada por");
-		textoEnlace.setFont(new Font("BatangChe", Font.PLAIN, 20));
-		textoEnlace.setForeground(Color.WHITE);
-		textoEnlace.setBounds(76, 503, 275, 59);
-		add(textoEnlace);
-		
-		//Enlace web
-		JLabel enlaceWeb = new JLabel("The Debrolopers");
-		enlaceWeb.setForeground(Color.WHITE);
-		enlaceWeb.setFont(new Font("BatangChe", Font.PLAIN, 20));
-		enlaceWeb.setBounds(350, 503, 161, 59);
-		add(enlaceWeb);
-		makeLink();
+		this.add(ing);
 		
 		//Label elegir idioma
 		JLabel lblEligeIdioma = new JLabel("Elige idioma");
 		lblEligeIdioma.setFont(new Font("BatangChe", Font.PLAIN, 18));
 		lblEligeIdioma.setForeground(Color.WHITE);
-		lblEligeIdioma.setBounds(207, 100, 125, 59);
-		add(lblEligeIdioma);
+		lblEligeIdioma.setBounds(623, 70, 125, 59);
+		this.add(lblEligeIdioma);
 		
 		//Imagen español
 		Image imgEspañol = new ImageIcon(this.getClass().getResource("/español.png")).getImage();
 		JLabel labelEsp = new JLabel("New label");
-		labelEsp.setBounds(76, 92, 100, 80);
+		labelEsp.setBounds(492, 62, 100, 80);
 		labelEsp.setIcon(new ImageIcon(imgEspañol));
 		this.add(labelEsp);
 		
 		//Label ingles
 		Image imgIngles = new ImageIcon(this.getClass().getResource("/ingles.png")).getImage();
 		JLabel labelIng = new JLabel("New label");
-		labelIng.setBounds(350, 92, 100, 80);
+		labelIng.setBounds(766, 62, 100, 80);
 		labelIng.setIcon(new ImageIcon(imgIngles));
 		this.add(labelIng);
-
+		
+		//Texto del enlace
+		JLabel textoEnlace = new JLabel("Aplicaci\u00F3n desarrollada por");
+		textoEnlace.setFont(new Font("BatangChe", Font.PLAIN, 20));
+		textoEnlace.setForeground(Color.WHITE);
+		textoEnlace.setBounds(475, 192, 275, 59);
+		this.add(textoEnlace);
+		
+		//Enlace web
+		enlaceWeb = new JTextField();
+		enlaceWeb.setForeground(SystemColor.textHighlight);
+		enlaceWeb.setBorder(null);
+		enlaceWeb.setVerifyInputWhenFocusTarget(false);
+		enlaceWeb.setOpaque(false);
+		enlaceWeb.setFont(new Font("BatangChe", Font.PLAIN, 20));
+		enlaceWeb.setBackground(UIManager.getColor("Button.background"));
+		enlaceWeb.setText("The Debrolopers");
+		enlaceWeb.setBounds(749, 192, 161, 59);
+		add(enlaceWeb);
+		
+		//Copyrigth
+		SimpleAttributeSet sas = new SimpleAttributeSet(); 
+		StyleConstants.setBold(sas, true);
+		StyleConstants.setItalic(sas, true);
+		StyleConstants.setFontSize(sas, 35);
+		JTextPane CopyRigth = new JTextPane();
+		CopyRigth.setBounds(475, 262, 453, 134);
+		add(CopyRigth);
+		CopyRigth.setEditable(false);
+		CopyRigth.setEnabled(false);
+		CopyRigth.setFont(new Font("BatangChe", Font.PLAIN, 21));
+		CopyRigth.setForeground(Color.WHITE);
+		CopyRigth.setOpaque(false);
+		CopyRigth.setText("Copyrigth \r\nTodas las imagenes usadas en nuestra aplicaci\u00F3n son propias y elaboradas por nuestro dise\u00F1ador gr\u00E1fico Jorge Ombuena");
+		CopyRigth.getStyledDocument().setCharacterAttributes(0, 9, sas, false);
+		
 		//IMAGEN DE FONDO
 		Image iFondo = new ImageIcon(this.getClass().getResource("/Fondo.png")).getImage();
 		JLabel imgFondo = new JLabel("/Fondo.png");
+		imgFondo.setBorder(null);
 		imgFondo.setBounds(0, 0, 1355, 600);
 		imgFondo.setIcon(new ImageIcon(iFondo));
 		this.add(imgFondo);
 		
-	}
-
-	private void makeLink() {
-		addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				try {
-					Desktop.getDesktop().browse(new URI("gamequiz.esy.es/web/descripcionNosotros.html"));
-				} catch (IOException | URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-			}
-
-			@Override
-			public void mouseExited(MouseEvent e) {
-				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-			}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		//JScrollPane
+		JScrollPane jscroll = new JScrollPane();
+		jscroll.setBounds(475, 310, 453, 86);
+		jscroll.setEnabled(false);
+		jscroll.setBorder(null);
+		this.add(jscroll);
 		
+		jscroll.setOpaque(false);
+		jscroll.getViewport().setOpaque(false);
 	}
+	
+private void makeLink() {
+	addMouseListener(new MouseListener() {
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			try {
+				Desktop.getDesktop().browse(new URI("gamequiz.esy.es/web/descripcionNosotros.html"));
+			} catch (IOException | URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	
+}
 }
