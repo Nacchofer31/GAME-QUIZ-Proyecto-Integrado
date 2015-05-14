d70a083d18c6609b174facbba4d96de67f517b94
Nacchofer31 <nachopad31@hotmail.com>
2015-05-14 20:15:32 +0200

    Imagenes escaladas

--- ProyectoInicial/src/vista/PantallaQuiz.java
+++ ProyectoInicial/src/vista/PantallaQuiz.java
@@ -25,11 +25,13 @@ public class PantallaQuiz extends JPanel {
 		setLayout(null);
 		
 		JTextPane textPregunta = new JTextPane();
+		textPregunta.setEnabled(false);
+		textPregunta.setEditable(false);
 		textPregunta.setForeground(Color.WHITE);
-		textPregunta.setFont(new Font("Tahoma", Font.PLAIN, 21));
-		textPregunta.setText(control.getApiQuiz().getDato(0, 1));
+		textPregunta.setFont(new Font("Batang", Font.BOLD, 24));
+		textPregunta.setText("> "+control.getApiQuiz().getDato(0, 1));
 		textPregunta.setOpaque(false);
-		textPregunta.setBounds(164, 69, 977, 91);
+		textPregunta.setBounds(164, 100, 977, 91);
 		add(textPregunta);
 		
 		JButton btnSiguiente = new JButton("Siguiente");
@@ -42,7 +44,7 @@ public class PantallaQuiz extends JPanel {
 		btnPregunta1.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnPregunta1.setContentAreaFilled(false);
 		btnPregunta1.setBorderPainted(false);
-		btnPregunta1.setBounds(164, 194, 977, 49);
+		btnPregunta1.setBounds(164, 202, 977, 49);
 		add(btnPregunta1);
 		
 		JButton btnPregunta2 = new JButton(control.getApiQuiz().getDato(0, 3));
@@ -50,7 +52,7 @@ public class PantallaQuiz extends JPanel {
 		btnPregunta2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnPregunta2.setContentAreaFilled(false);
 		btnPregunta2.setBorderPainted(false);
-		btnPregunta2.setBounds(164, 254, 977, 49);
+		btnPregunta2.setBounds(164, 262, 977, 49);
 		add(btnPregunta2);
 		
 		JButton btnPregunta3 = new JButton(control.getApiQuiz().getDato(0, 4));
@@ -58,7 +60,7 @@ public class PantallaQuiz extends JPanel {
 		btnPregunta3.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnPregunta3.setContentAreaFilled(false);
 		btnPregunta3.setBorderPainted(false);
-		btnPregunta3.setBounds(164, 314, 977, 49);
+		btnPregunta3.setBounds(164, 322, 977, 49);
 		add(btnPregunta3);
 		
 		JButton btnPregunta4 = new JButton(control.getApiQuiz().getDato(0, 5));
@@ -66,7 +68,7 @@ public class PantallaQuiz extends JPanel {
 		btnPregunta4.setForeground(Color.WHITE);
 		btnPregunta4.setContentAreaFilled(false);
 		btnPregunta4.setBorderPainted(false);
-		btnPregunta4.setBounds(164, 367, 977, 49);
+		btnPregunta4.setBounds(164, 382, 977, 49);
 		add(btnPregunta4);
 		JLabel imgFondo = new JLabel("/Fondo.png");
 		imgFondo.setBounds(0, 0, 1355, 600);
